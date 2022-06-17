ee148e861921a88770cdbdb372f1cdcb5b5cb652
Luca Barone <baro.luc@gmail.com>
2022-06-17 23:56:29 +0200

    Add create todo form

--- apps/remix/app/routes/index.tsx
+++ apps/remix/app/routes/index.tsx
@@ -1,18 +1,33 @@
-import type {LoaderFunction} from '@remix-run/node'
-import {useLoaderData} from '@remix-run/react'
 import type {GetTodosDTO} from 'api-contract'
+import type {ActionFunction, LoaderFunction} from '@remix-run/node'
+import {Form, useLoaderData} from '@remix-run/react'
 
 type LoaderData = GetTodosDTO['response']
 
-export const loader: LoaderFunction = async ({request}) => {
+export const loader: LoaderFunction = async () => {
   const res = await fetch(`${process.env.API_ORIGIN}/todos`)
+  if (!res.ok) {
+    throw res
+  }
+  const body: LoaderData = await res.json()
+  return body
+}
+
+export const action: ActionFunction = async ({request}) => {
+  const form = await request.formData()
+  const text = form.get('text')
+
+  const res = await fetch(`${process.env.API_ORIGIN}/todos`, {
+    method: request.method,
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({text}),
+  })
 
   if (!res.ok) {
     throw res
   }
 
-  const body: LoaderData = await res.json()
-  return body
+  return null
 }
 
 export default function Index() {
@@ -20,6 +35,10 @@ export default function Index() {
   return (
     <>
       <h1>todos</h1>
+      <Form method="post">
+        <input type="text" name="text" />
+        <button type="submit">Add todo</button>
+      </Form>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   )
