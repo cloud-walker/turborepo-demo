69554a67f6633aeb2d33ebf9fd8f7631301c2cc7
Luca Barone <baro.luc@gmail.com>
2022-06-17 23:50:21 +0200

    Load todos from remix

--- apps/remix/app/routes/index.tsx
+++ apps/remix/app/routes/index.tsx
@@ -1,32 +1,23 @@
+import type {LoaderFunction} from '@remix-run/node'
+import {useLoaderData} from '@remix-run/react'
+
+export const loader: LoaderFunction = async ({request}) => {
+  const res = await fetch(`${process.env.API_ORIGIN}/todos`)
+
+  if (!res.ok) {
+    throw res
+  }
+
+  const body = await res.json()
+  return body
+}
+
 export default function Index() {
+  const data = useLoaderData()
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <>
+      <h1>todos</h1>
+      <pre>{JSON.stringify(data)}</pre>
+    </>
+  )
 }
